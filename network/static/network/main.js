5b1ef4fbf0ed9c4e8af2a16d1eeba5302e685340
Davidngy <davidngy2803gmail.com>
2024-01-12 00:35:06 +0100

    these are mc changes

--- network/static/network/main.js
+++ network/static/network/main.js
@@ -39,47 +39,33 @@ function updateLikeButton(postID, data) {
 document.querySelectorAll(".edit").forEach(function (editButton) {
   editButton.addEventListener("click", function (event) {
     const postID = event.target.dataset.postId;
-    const postContent = event.target.dataset.postContent;
 
-    updateEditButton(postID, postContent);
+    updateEditButton(postID);
   });
 });
 //here the right text area appears and you are able to make changes
 function updateEditButton(postID) {
   // declare right value to each variable
-
   const editContainer = document.getElementById(`edit-container-${postID}`);
   let textarea = document.getElementById(`edit-textarea-${postID}`);
   const saveChangesButton = document.getElementById(`saveChangesButton-${postID}`);
-
-  if (!editContainer || !textarea || !saveChangesButton) {
-    console.error("One or more elements not found.");
   
-  } else {
-    fetch(`/textarea/${postID}/`)
-        .then(response => response.json())
-        .then(content => {
-            console.log("Content from server:", content); // Log the entire content object
+fetch(`/textarea/${postID}/`)
+    .then(response => response.json())
+    .then(content => {
             
-            document.getElementById(`post-content-${postID}`).style.display = "none";
-            editContainer.style.display = "block";
-            textarea.value = content.content;
+        document.getElementById(`post-content-${postID}`).style.display = "none";
+        editContainer.style.display = "block";
+        textarea.value = content.content;
 
-            saveChangesButton.addEventListener("click", function () {
-                const updatedContent = textarea.value;
-                saveChanges(postID, updatedContent, editContainer);
-            });
-        })
-        .catch(error => console.error("Error fetching data:", error));
-        
-  }
+        saveChangesButton.addEventListener("click", function () {
+            const updatedContent = textarea.value;
+            saveChanges(postID, updatedContent, editContainer);
+        });
+    })
 }
 //here you save the changes to django
 function saveChanges(postID, updatedContent, editContainer) {
-
-  console.log('updatedContentBeChange:', updatedContent);
-  document.getElementById(`edit-textarea-${postID}`).innerHTML = updatedContent;
-  document.getElementById(`post-content-${postID}`).style.display = "none";
   const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
   
   // we send a request to our backend to update the content
@@ -95,21 +81,14 @@ function saveChanges(postID, updatedContent, editContainer) {
   })
     .then(response => response.json())
     .then(updatedPost => {
-      // document.getElementById(`post-content-${postID}`).innerHTML = updatedPost.post_content;
       editContainer.style.display = "none";
-      
-      
 
-      // update the frontend to have the updated content
       let postContent = document.getElementById(`post-content-${postID}`)
       postContent.innerText = updatedPost.post_content;
-
-      console.log('This should showing' + 'updatedContentEnd:', updatedContent);
-      document.getElementById(`post-content-${postID}`).style.display = "block";
-
-      //hide the textarea and show the post content
       
-
+      document.getElementById(`post-content-${postID}`).style.display = "block";
+     
+      //might update the user interface to reflect the changes
     });
 }
 
